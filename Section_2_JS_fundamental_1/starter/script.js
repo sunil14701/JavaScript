4596688e1da1bc6b5c1e609746f49733bb703a32
Sunil.Kumar <sunilkumar333221@gmail.com>
2024-09-04 14:01:14 +0530

    s2 l9 completed

--- Section_2_JS_fundamental_1/starter/script.js
+++ Section_2_JS_fundamental_1/starter/script.js
@@ -1,6 +1,37 @@
-let js = "amazing";
+// 8:linking a JS file
+// let js = "amazing";
 
-if (js === "amazing") alert("JS is FUN!!!");
-else alert("Boring");
+// if (js === "amazing") alert("JS is FUN!!!");
+// else alert("Boring");
 
-console.log(2 * 3);
+// console.log(2 * 3);
+
+//9: values and variables
+// value is smallest unit of info in js like "jonas". values can be stored them in variables
+// variable: it is a box in which we can store ta value.
+console.log("Jonas");
+
+// camelCase notation: standard notation in JS
+let firstName = "Bob";
+console.log(firstName);
+
+let first_name_person = "sunil"; // snake case notation
+
+// variable name can not start with number it can only contain letters, numbers, underscores or dollar sign
+// let 3years = 3; can't write number prefix
+// let jonas&metilda = 'JM'; wrong
+// let new = 10; reserved varibale name are not allowed eg: function
+let _function = 23;
+let $function = 23;
+let name = "Jonas"; // it is reserved keyword but still we can use
+let Person = "Jonas"; //starting with upper case only for OOPS'S
+let PI = 3.14; // upper case variable case for variable that is constant
+
+// make variable name as descriptive as possible
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob, myCurrentJob);
