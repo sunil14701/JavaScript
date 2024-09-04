14b35c9e27f9b5b811c7838772042193cb6eae6a
Sunil.Kumar <sunilkumar333221@gmail.com>
2024-09-04 22:35:43 +0530

    s2 l11 end

---
+++ Section_2_JS_fundamental_1/starter/assignment.js
@@ -0,0 +1,8 @@
+// Assignament 1: values and variables
+let country = "India";
+let continent = "Asia";
+let population = 1500; // in millions
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- Section_2_JS_fundamental_1/starter/script.js
+++ Section_2_JS_fundamental_1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 // 8:linking a JS file
 // let js = "amazing";
 
@@ -5,7 +6,9 @@
 // else alert("Boring");
 
 // console.log(2 * 3);
+*/
 
+/*
 //9: values and variables
 // value is smallest unit of info in js like "jonas". values can be stored them in variables
 // variable: it is a box in which we can store ta value.
@@ -35,3 +38,42 @@ let job1 = "Programmer";
 let job2 = "Teacher";
 
 console.log(myFirstJob, myCurrentJob);
+*/
+
+/*
+//: 11 data types
+//  in js a value is only object(dict) or primitive datatype
+
+// there are 7 primitive data types
+// 1. Number: stores float or int
+// 2. string: text
+// 3. boolean: true or fasle
+// 4. undefined: varibale declared but not inilitized -> empty value
+// 5. Null: empty value -> declared and inltilized
+// 6. symbol(ES2015): value that is unique and cannot be changed
+// 7.BigInt: Larger integer than the number type can hold
+
+// JS is dynamic typing. ``values have type not variable``. assing diff values with diff datatype within same variable in single program
+
+let JsIsFun = true;
+let age = 23;
+let firstName = "Jonas";
+console.log(JsIsFun, age, firstName);
+console.log(typeof JsIsFun); // typeof is operator not a func
+console.log(typeof typeof JsIsFun); // string
+
+// dynamic typing
+JsIsFun = "Yes";
+console.log(typeof JsIsFun);
+// let JsIsFun = 12; already declared
+
+
+// undefined with both value and its type
+let secondName;
+console.log(secondName); // undefined
+console.log(typeof secondName); // undefined
+let email = null;
+console.log(typeof email); // object, value is null but not type, bug not corrected bcs of legacy reasons
+*/
+
+// 12 let, const and var
