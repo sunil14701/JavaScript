711b2c3b7ad9fbb906ef4e383d51640afcce1b89
Sunil.Kumar <sunilkumar333221@gmail.com>
2024-09-07 22:38:24 +0530

    s3 l33 end

--- Section_3_JS_fundamentals_2/starter/script.js
+++ Section_3_JS_fundamentals_2/starter/script.js
@@ -1,8 +1,10 @@
 'use strict';
-
+/*
 // l31 section intro
 console.log(`Hello world`);
+*/
 
+/*
 // l32: activating strict mode
 // make js cide secure. 'use strict'; *MUST be first line of js file.
 // why strict mode?
@@ -21,3 +23,31 @@ const private = 23;
 // const if = 23;
 
 // more on this later; such as for functions, objecs, setting properties on primitive values etc.
+*/
+
+// l33: functions
+// peice of code used again and again.
+// DRY principle; maintainable code
+function logger(){
+    console.log(`My name is jonas`);
+}
+
+// calling/running/invoking a fx
+logger();
+logger();
+
+// parametrized fx, arguments
+function fruitProcessor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with  ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const juice = fruitProcessor(5, 0);
+const juice2 = fruitProcessor(5, 4);
+const log = logger(12,23,34);// specifying args will not have any effect
+console.log(juice);
+console.log(juice2);
+console.log(log);
+
+// console.log(), Number(), boolean() -> built-in fx
