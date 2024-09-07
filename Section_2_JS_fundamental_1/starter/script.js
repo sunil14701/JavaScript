a2ce37be2a4c983d4a5efc7faea441c527731780
Sunil.Kumar <sunilkumar333221@gmail.com>
2024-09-07 16:27:53 +0530

    s2 l26 end

--- Section_2_JS_fundamental_1/starter/script.js
+++ Section_2_JS_fundamental_1/starter/script.js
@@ -315,4 +315,57 @@ if(canDrive) console.log(`Sarah can drive for next 2 hrs`);
 else console.log(`Sarah has to rest.`);
 */
 
-// 
+// L26 switch statement
+let day = prompt("Enter you Day below");
+
+if (day == "1") day = Number(day);
+
+switch (day) {
+  case "Monday": // day === 'Monday' i.e strict comparing
+    console.log(`Today is monday`);
+    break;
+
+  case "Tuesday":
+    console.log(`Today is Tuesday`);
+    break;
+
+  case "Wednesday":
+    console.log(`Today is Wednesday`);
+    break;
+
+  case "Thrusday":
+    console.log(`Today is Thrusday`);
+    break;
+
+  case "Friday":
+    console.log(`Today is Friday`);
+    break;
+
+  case "Saturday":
+    console.log(`Today is Saturday`);
+    break;
+
+  case "1":
+    console.log(`1 is string`);
+    break;
+
+  case 1:
+    console.log(`1 is number`);
+    break;
+
+  case "Sunday":
+    console.log(`Today is Sunday`);
+    break;
+
+  default:
+    console.log("Default");
+}
+
+if (day === "Monday") console.log("Today is Monday");
+else if (day === "Tuesday") console.log("Today is Tuesday");
+else if (day === "Wednesday") console.log("Today is wednesday");
+else if (day === "Thursday") console.log("Today is Thursday");
+else if (day === "Friday") console.log("Today is Friday");
+else if (day === "Saturday") console.log("Today is Saturday");
+else if (day === "Sunday") console.log("Today is Sunday");
+else console.log("Default");
