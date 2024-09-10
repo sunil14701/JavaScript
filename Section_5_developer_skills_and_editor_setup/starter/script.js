fb3af63bb7b1da520771e0c772a93173aaf432dc
Sunil.Kumar <sunilkumar333221@gmail.com>
2024-09-10 22:10:53 +0530

    l56 end

--- Section_5_developer_skills_and_editor_setup/starter/script.js
+++ Section_5_developer_skills_and_editor_setup/starter/script.js
@@ -1,6 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
 // l55: setting up prettier and vs code
 // 1. prettier vs extention: go to docs to customise the coding style. eg: single brackets instead of double brackets
 
@@ -15,5 +16,18 @@ console.log(x);
 // 2. auto close tag vs extention, ESlint extention, image preview, todo highlight
 // BUG, note, todo, lecture
 
+*/
 
+/*
+// l56: install node.js and setting up a dev env
+//  live server extention vs extention
+console.log(237);
 
+// real js programmer use this
+// nodejs : js runtime, run js outside the browser
+// node -v, npm --version
+// npm install live-server -g // npm node package manager -> program to download tools, -g -> globally
+// in terminal -> live-server
+*/
+
+// l57: learning how to code
