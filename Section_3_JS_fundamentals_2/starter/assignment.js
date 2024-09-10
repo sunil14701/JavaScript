b6f90067aa3216ad579a347b454858b1fb8c6c5b
Sunil.Kumar <sunilkumar333221@gmail.com>
2024-09-10 21:48:26 +0530

    l55 end

--- Section_3_JS_fundamentals_2/starter/assignment.js
+++ Section_3_JS_fundamentals_2/starter/assignment.js
@@ -70,6 +70,79 @@ console.log(populations);
 console.log(percentages);
 
 // Assignment 5: basic array operations
+let neigbours = ['China','Pakistan','Myanmar','Srilanka'];
+neigbours.push('Utopia');
+neigbours.pop();
+if(!neigbours.includes('Germany')){
+    console.log(`Probably no a central european country :D`);
+}
+const index = neigbours.indexOf('China');
+neigbours[index] = 'Republic of China';
+console.log(neigbours);
 
+// assgnment 6: intro to objects
+const myCountry = {
+    country: 'India',
+    capital: 'Delhi',
+    language: 'Hindi',
+    population : 1450,
+    neigbours: neigbours,
+    // describe: function(){
+    //     const desc = `${this.country} has ${this.population} ${this.language}-speaking people, ${this.neigbours.length} neighbourning countries and a capital called ${this.capital}.`
+    //     return desc;
+    // }
+}
+console.log(myCountry);
 
+// asssignment 7: dot vs bracket notation
 
+console.log(`${myCountry.country} has ${myCountry.population} ${myCountry.language}-speaking people, ${myCountry.neigbours.length} neighbourning countries and a capital called ${myCountry.capital}.`);
+
+console.log(myCountry.population)
+myCountry.population+=2;
+console.log(myCountry.population)
+myCountry['population']-=2;
+console.log(myCountry.population)
+
+// assignment 8: object methods
+myCountry.describe =  function(){
+        const desc = `${this.country} has ${this.population} ${this.language}-speaking people, ${this.neigbours.length} neighbourning countries and a capital called ${this.capital}.`
+        return desc;
+    };
+console.log(myCountry.describe());
+
+myCountry.checkIsland = !myCountry.neigbours.length? true:false;
+console.log(myCountry.checkIsland);
+
+// assignment 8
+for(let i=0;i<50;i++){
+    console.log(`Voter number ${i+1} is currently voting.`);
+}
+
+// assignment 9
+const percentage2 = [];
+for(let i=0;i<populations.length;i++){
+    percentage2.push(percentageOfWorld1(populations[i]));
+}
+console.log(percentages);
+console.log(percentage2);
+
+// assignemnt 10: looping backwards and loops in loop
+let listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
+
+for(let i=0;i<listOfNeighbours.length;i++){
+    for(let j=0;j<listOfNeighbours[i].length;j++){
+        console.log(`Neighbour: ${listOfNeighbours[i][j]}`);
+    }
+}
+
+// assignment 11:while loop
+let i=0;
+const percentage3 = [];
+while(i<populations.length)
+    {
+        percentage3.push(percentageOfWorld1(populations[i]));
+        i++;
+    }
+console.log(percentage3);
+// for loop is better as we know len of array prev
---
+++ Section_5_developer_skills_and_editor_setup/starter/script.js
@@ -0,0 +1,19 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+// l55: setting up prettier and vs code
+// 1. prettier vs extention: go to docs to customise the coding style. eg: single brackets instead of double brackets
+
+const x = 23;
+if (x === 45) console.log(`hello`);
+
+const calcAge = (birthYear) => 2037 - birthYear;
+
+// global snippets
+console.log(x);
+
+// 2. auto close tag vs extention, ESlint extention, image preview, todo highlight
+// BUG, note, todo, lecture
+
+
+
