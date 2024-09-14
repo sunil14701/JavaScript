0232a2de939f8b0ad6f3b82e765e54cf48331b2b
Sunil.Kumar <sunilkumar333221@gmail.com>
2024-09-14 12:15:27 +0530

    l71 ended

--- Section_7_JS_in_browser_DOM_and_event_fundamentals/starter/script.js
+++ Section_7_JS_in_browser_DOM_and_event_fundamentals/starter/script.js
@@ -1,5 +1,20 @@
 'use strict';
 
+/*
 // l70: project 1:guess my number
+
+// select html class 
 console.log(document.querySelector('.message').textContent);
+
+// select html id 
+// console.log(document.querySelector('#message').textContent);
 //  console.log(document);
+*/
+
+// l71: DOM and DOM manipulation
+// DOM: document object model, complete html in tree like structure
+// DOM is not part of JS, but part of web API.
+// web API: librabry implemented by browsers, auto ovailbale to js without importing anything.
+// besides DOM there are other webAPI's like timers fetch
+// pdf 
+
