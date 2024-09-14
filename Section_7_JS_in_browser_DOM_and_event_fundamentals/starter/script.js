6e9ec2e77eb779f4f76eaa74c053db114c6887cf
Sunil.Kumar <sunilkumar333221@gmail.com>
2024-09-14 13:03:08 +0530

    l73 ended

--- Section_7_JS_in_browser_DOM_and_event_fundamentals/starter/script.js
+++ Section_7_JS_in_browser_DOM_and_event_fundamentals/starter/script.js
@@ -16,11 +16,34 @@ console.log(document.querySelector('.message').textContent);
 // DOM is not part of JS, but part of web API.
 // web API: librabry implemented by browsers, auto ovailbale to js without importing anything.
 // besides DOM there are other webAPI's like timers fetch
-// pdf 
+// pdf
 
+/*
 // l72 selecting ana manupulating elements
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number';
 document.querySelector('.number').textContent = '12';
 document.querySelector('.score').textContent = 13;
-document.querySelector('.guess').value = 34;// take input
+document.querySelector('.guess').value = 34; // take input
+
+*/
+// l73: handling click events
+
+
+
+document.querySelector('.check').addEventListener('click', 
+    // event handler
+    function () {
+//   console.log(document.querySelector('.guess').value);
+  const inputValue = Number(document.querySelector('.guess').value);
+  console.log(inputValue,typeof inputValue);
+    if(!inputValue){
+        document.querySelector('.message').textContent = 'No number entered.'
+    }else{
+        
+    }
+    // if(inputValue  === 45) {
+    //     document.querySelector('.message').textContent = 'Sunil Wins';
+    // }
+});
+// addeventlistener: js engine executes the fx, we just pass the fx expression and do not call it 
