e8c41ebf8954ea2eefbd835787f8af27c4f80ab3
Sunil.Kumar <sunilkumar333221@gmail.com>
2024-09-15 23:03:29 +0530

    l90 ended

---
+++ Section_8_how_js_works_behind_the_scenes/starter/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// l89: a high-level overview of JS
+
+// 1. JS is a high-lvl object oriented, multi-paradigm, programming lang.
+
+// 2. JS is a high-lvl, prototype based object oriented, multi-paradigm, interpreted or Just-in-time compiled, dynamic, single threaded, garbage-collected programming lang with first class fxs and non-blocking event loop concurrency model.
+
+// high-lvl: do not manage memory manually, programs will never be as fast/optimise as low lvl lang like c lang.
+
+// garbage collected: a algo in js engine which auto removes the old/unused object from memory. we do not clean our memory manually.
+
+// interpreted or just in time compiled: execute code line by line, do not first compile than executes.
+
+// multi-paradigm: paradigm-> an approach and mindset of structureing code, which will direct your coding style and technique.
+// 3 popular paradigm are :
+// a. procedural programming-> code in linear way i.e linear + fxs. we are doing this so far.
+// b.OOP's
+// c. functional programming
+// classfied as imperative or declarive
+
+// prototype-based object-oriented: prototype -> blueprint. eg-> arrays methods are inherated from array prototype. every thing in js is object except primitive datatypes
+
+// first class fx: fxs can be treated as variables. can be pass to other fxs and return them from fxs.
+
+// dynamic typing: do not have to declare varibale datatype. type of varibale can easily be changed at runtime. js with types -> typescript lang
+
+// single threaded: js runs in one single thread, can do one thing at a time. thread: set of instructions that is executed in the computer CPU. thread is where our code is executed in machines processors. non-blocking beaviou  is achieved by event loop. event loop takes long running tasks,executes them in background and put them back in the main thread once they are finished.
+
+// concurrency model : handling multiple task at same time.
